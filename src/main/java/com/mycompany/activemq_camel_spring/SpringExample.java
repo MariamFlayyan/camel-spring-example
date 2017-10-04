0e8e952f8a8bed76c80d3b8920b51aaa8e82cbfd
Colin But <colinbut@users.noreply.github.com>
2017-10-04 11:20:45 +0100

    Showing how to setup spring with a test bean

---
+++ src/main/java/com/mycompany/activemq_camel_spring/SpringExample.java
@@ -0,0 +1,26 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.activemq_camel_spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringExample {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringExample.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        try {
+            TestBean testBean = (TestBean) applicationContext.getBean("testBean");
+            LOGGER.info(testBean.hello("Camel and Spring"));
+        } finally {
+            applicationContext.close();
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/activemq_camel_spring/TestBean.java
@@ -0,0 +1,13 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.activemq_camel_spring;
+
+public class TestBean {
+
+    public String hello(String msg) {
+        return "Hello " + msg;
+    }
+}
