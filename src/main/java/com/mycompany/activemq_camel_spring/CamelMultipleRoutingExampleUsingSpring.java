0313104b6ed5ba4434a93a018773e360eb820fb4
Colin But <colinbut@users.noreply.github.com>
2017-10-04 23:28:33 +0100

    multiple routing

---
+++ src/main/java/com/mycompany/activemq_camel_spring/CamelMultipleRoutingExampleUsingSpring.java
@@ -0,0 +1,28 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.activemq_camel_spring;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CamelMultipleRoutingExampleUsingSpring {
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("multipleRoutingApplicationContext.xml");
+        CamelContext camelContext = SpringCamelContext.springCamelContext(applicationContext, false);
+
+        try {
+            camelContext.start();
+            Thread.sleep(4000);
+        } finally {
+            camelContext.stop();
+            applicationContext.close();
+        }
+
+    }
+}
