6f05f1f70d0b2334cac6094bd5bafb47fcba4a5d
Colin But <colinbut@users.noreply.github.com>
2017-10-04 11:34:49 +0100

    Example using Java DSL configuring routes for activemq in Camel

---
+++ src/main/java/com/mycompany/activemq_camel_spring/CamelActiveMQExample.java
@@ -0,0 +1,48 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.activemq_camel_spring;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.util.jndi.JndiContext;
+
+public class CamelActiveMQExample {
+
+    public static void main(String[] args) throws Exception {
+        JndiContext jndiContext = new JndiContext();
+        jndiContext.bind("testBean", new TestBean());
+
+        CamelContext camelContext = new DefaultCamelContext(jndiContext);
+        camelContext.addComponent("activemq", ActiveMQComponent.activeMQComponent("vm://localhost?broker.persistent=false"));
+
+        try {
+            camelContext.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("activemq:queue:start")
+                        .to("bean:testBean?method=hello")
+                        .to("stream:out");
+                }
+            });
+
+            ProducerTemplate producerTemplate = camelContext.createProducerTemplate();
+            camelContext.start();
+
+            for (int i = 0; i < 5; i++) {
+                producerTemplate.sendBody("activemq:queue:start", "body" + i);
+            }
+
+            Thread.sleep(1000);
+
+        } finally {
+            camelContext.stop();
+        }
+
+    }
+}
