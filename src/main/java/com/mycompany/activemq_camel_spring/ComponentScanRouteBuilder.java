a30c6ac1dee09ef3fd669e76570eb1b52ed5315c
Colin But <colinbut@users.noreply.github.com>
2017-10-05 09:49:36 +0100

    Route building using Spring Component Scan

---
+++ src/main/java/com/mycompany/activemq_camel_spring/ComponentScanRouteBuilder.java
@@ -0,0 +1,21 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.activemq_camel_spring;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentScanRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("timer:foo?period=1s")
+            .transform()
+            .simple("Heartbeat ${date:now:yyyy-MM-dd HH:mm:ss}")
+            .to("stream:out");
+    }
+}
