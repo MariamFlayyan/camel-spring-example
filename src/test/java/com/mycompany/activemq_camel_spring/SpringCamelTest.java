5a278137a6eac8bcbe7daa499bec844fa256a28c
Colin But <colinbut@users.noreply.github.com>
2017-10-07 21:24:34 +0100

    Added comments

--- src/test/java/com/mycompany/activemq_camel_spring/SpringCamelTest.java
+++ src/test/java/com/mycompany/activemq_camel_spring/SpringCamelTest.java
@@ -31,16 +31,22 @@ public class SpringCamelTest extends TestCase {
 
     @Test
     public void testConsumerTemplate() throws InterruptedException {
+        // expect the message to be received mock
         mock.expectedBodiesReceived("Hello World");
 
+        // send message to seda:start endpoint
         producer.sendBody("seda:start", "Hello World");
 
+        // then consume the message from seda:start
         String body = consumer.receiveBody("seda:start", String.class);
 
         assertEquals("Hello World", body);
 
+        // send the message again to seda:foo so it would be routed to the mock endpoint so our unit test
+        // can complete
         producer.sendBody("seda:foo", body);
 
+        // assert mock got the message
         mock.assertIsSatisfied();
     }
 }
